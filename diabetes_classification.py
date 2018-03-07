2c6c4ea7dd0b402a06a793ab41afbad47a9c19de
Svanik <34766427+Svanik@users.noreply.github.com>
2018-03-06 19:22:08 -0500

    Modfied Nueral Net Architecture to reach higher accuracy also a train/test split

--- diabetes_classification.py
+++ diabetes_classification.py
@@ -1,5 +1,6 @@
 from keras.models import Sequential
 from keras.layers import Dense
+from sklearn.model_selection import train_test_split
 import numpy
 
 # random seed for reproducibility
@@ -10,15 +11,14 @@ dataset = numpy.loadtxt("prima-indians-diabetes.csv", delimiter=",")
 # split into input (X) and output (Y) variables, splitting csv data
 X = dataset[:, 0:8]
 Y = dataset[:, 8]
-
+x_train, x_validation, y_train, y_validation = train_test_split(
+    X, Y, test_size=0.45, random_state=5)
 # create model, add dense layers one by one specifying activation function
 model = Sequential()
-model.add(Dense(8, input_dim=8, activation='relu'))  # input layer requires input_dim param
-model.add(Dense(16, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(16, activation='relu'))
-model.add(Dense(8, activation='relu'))
-model.add(Dense(4, activation='relu'))
+model.add(Dense(10, input_dim=8, activation='relu'))  # input layer requires input_dim param
+model.add(Dense(50, activation='relu'))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(5, activation='relu'))
 # sigmoid instead of relu for final probability between 0 and 1
 model.add(Dense(1, activation='sigmoid'))
 
@@ -26,8 +26,10 @@ model.add(Dense(1, activation='sigmoid'))
 model.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
 
 # call the function to fit to the data (training the network)
-model.fit(X, Y, epochs=1000, batch_size=10)
+model.fit(x_train, y_train, epochs=1000, batch_size=10,
+          validation_data=(x_validation, y_validation))
 
 # evaluate the model
+
 scores = model.evaluate(X, Y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
